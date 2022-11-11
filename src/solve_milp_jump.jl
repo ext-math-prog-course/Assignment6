5d049a4eca26d870341e86f80e295199429486f4
Forrest Laine <forrest.laine@vanderbilt.edu>
2022-11-11 01:06:51 -0600

    removing solutions

--- src/solve_milp_jump.jl
+++ src/solve_milp_jump.jl
@@ -11,15 +11,8 @@ s.t.        l ≤ Ax ≤ u
 Returns named tuple (; x1, x2)
 """
 function solve_milp_jump(n1, n2, q, A, l, u)
-    model = Model(HiGHS.Optimizer)
-    m = length(l)
-    @variable(model, x1[1:n1])
-    @variable(model, x2[1:n2], Int)
-    @variable(model, l[i]≤s[i=1:m]≤u[i])
-    @constraint(model, con, A*[x1;x2]-s .== 0)
-    @objective(model, Min, q'*[x1; x2])
-    optimize!(model)
-    return (; x1=value.(x1), x2=value.(x2)) 
+    # todo 
+    return (; x1=zeros(Float64, n1), x2=zeros(Int, n2)) 
 end
 
 """
@@ -40,30 +33,11 @@ This format will be required for testing purposes.
 returns named tuple (; n1, n2, q, A, l, u).
 """
 function burrito_to_standard_form(d, α, f, r, k)
-    n, m = size(α)
-    num_vars = m*(n+1)
-    A = spzeros((m+1)*n+num_vars, num_vars)
-    for j = 1:m
-        for i = 1:n
-            A[(j-1)*n+i, j] = 1.0
-            A[(j-1)*n+i, j+i*m] = -1.0
-        end
-    end
-    for i = 1:n
-        A[n*m+i,m*i+1:m*(i+1)] .= 1.0
-    end
-    A[(m+1)*n+1:end,:] = I(num_vars)
-    u = [fill(Inf, n*m); ones(n); ones(num_vars)]
-    l = [zeros(n*m); fill(-Inf, n); zeros(num_vars)]
-    q = zeros(num_vars)
-    q[1:m] .= -f
-    for i = 1:n
-        for j = 1:m
-            q[i*m+j] = (r-k)*α[i,j]*d[i]
-        end
-    end
-    q *= -1.0
-    n1 = 0
-    n2 = n*m+m
+    # todo, fix
+    n1 = n2 = 0
+    q = zeros(n1+n2)
+    A = spzeros(0, n1+n2)
+    l = zeros(0)
+    u = zeros(0)
     return (; n1, n2, q, A, l, u)
 end
